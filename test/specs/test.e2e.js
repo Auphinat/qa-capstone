37646eae4ca40411bebfdfb015b22518b868ca43
Auphinat <auph1448@gmail.com>
2025-11-05 11:03:45 -0700

    Test

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,7 +2,7 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
 
-// Testing: Game page, Search, Mod page, Surprise me
+// Testing: Game page, Search, Mod page, Surprise me 
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
